a935cf5da34ddbd5e1c07c6f7f19f63b234f9827
Minaholics <admin@minaholics.com>
2014-06-15 21:05:33 -0400

    New configuration file.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.clientid =  'obubzmzbhjnvaai1cgedgqbrbd75a1k';
-config.clientsecret = 'j6dbst6mri9h8pan8q7n4kxku5kr95j';
+config.clientid =  '';
+config.clientsecret = '';
 config.callbackurl =  'http://127.0.0.1/auth/twitch/callback';
 
 config.port =  80;
