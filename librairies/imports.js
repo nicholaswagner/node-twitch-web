7d9ebbea6193c99b72ac916a706b5d7ae7c44d63
Minaholics <admin@minaholics.com>
2014-06-15 21:09:32 -0400

    Better imports.

--- librairies/imports.js
+++ librairies/imports.js
@@ -1,30 +1,14 @@
-var config = require('../config'),
-	http = require('http'),
-	passport = require('passport'),
-	path = require('path'),
-	strategy = require('./twitch').Strategy,
-	
-	// Express modules.
-	bodyparser = require('body-parser'),
-	cookieparser = require('cookie-parser'),
-	ejslocals = require('ejs-locals'),
-	favicon = require('serve-favicon'),
-	express = require('express'),
-	method = require('method-override'),
-	session = require('express-session');
-
-
-exports.config = config;
-exports.http = http;
-exports.passport = passport;
-exports.path = path;
-exports.strategy = strategy;
+exports.config = require('../config');
+exports.http = require('http');
+exports.passport = require('passport');
+exports.path = require('path');
+exports.strategy = require('./twitch').Strategy;
 
 //Express modules.
-exports.bodyparser = bodyparser;
-exports.cookieparser = cookieparser;
-exports.ejslocals = ejslocals;
-exports.favicon = favicon;
-exports.express = express;
-exports.method = method;
-exports.session = session;
+exports.bodyparser = require('body-parser');
+exports.cookieparser = require('cookie-parser');
+exports.ejslocals = require('ejs-locals');
+exports.favicon = require('serve-favicon');
+exports.express = require('express');
+exports.method = require('method-override');
+exports.session = require('express-session');
